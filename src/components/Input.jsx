d41e5a7152a682527e3a9ea54d002502bbe01043
MarkBjj <markmullendev@gmail.com>
2026-08-10 20:32:05 -0300

    Tailwind style on Input fields

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,9 +1,18 @@
 export default function Input({ textarea, label, ...props }) {
   textarea = textarea || false;
+  const fieldStyle =
+    "w-full p-2 border-b-2 border-slate-300 rounded bg-stone-300 text-stone-600 focus-outline-none  focus-border-stone focus:border-stone-600";
   return (
-    <p>
+    <p className="flex flex-col gap-1 my-2">
       <label>{label}</label>
-      {textarea ? <textarea {...props} /> : <input {...props} />}
+      {textarea ? (
+        <textarea className={fieldStyle} {...props} />
+      ) : (
+        <input
+          className={fieldStyle}
+          {...props}
+        />
+      )}
     </p>
   );
 }
--- src/components/NewProject.jsx
+++ src/components/NewProject.jsx
@@ -2,28 +2,26 @@ import Input from "./Input";
 
 export default function NewProject() {
   return (
-    <div className="w-[14rem] bg-transparent p-4">
-      <menu className="flex flex-col gap-2">
+    <div className="w-[35rem] mt-16 bg-transparent p-4">
+      <menu className="flex items-center justify-end gap-4 my-4">
         <li>
-          <button className="px-2 py-1 text-xs md:text-base uppercase bg-green-500 text-white rounded hover:bg-stone-900 hover:text-slate-400">
+          <button className="px-2 py-2 text-xs md:text-base uppercase bg-green-500 text-white rounded hover:bg-stone-900 hover:text-slate-400">
             Save
           </button>
         </li>
         <li>
-          <button className="px-1 py-2 my-2 text-xs md:text-base uppercase bg-red-500 text-white rounded hover:bg-stone-900 hover:text-slate-400">
+          <button className="px-6 py-2 my-2 text-xs md:text-base uppercase bg-red-500 text-white rounded hover:bg-stone-900 hover:text-slate-400">
             Cancel
           </button>
         </li>
-
-        <div>
-          Title
-          <Input label="Title" />
-          Description
-          <Input label="Description" textarea />
-          Due Date
-          <Input label="Due Date" type="date" />
-        </div>
       </menu>
+      <div>
+        <Input label="Title" />
+
+        <Input label="Description" textarea />
+
+        <Input label="Due Date" type="date" />
+      </div>
     </div>
   );
 }
