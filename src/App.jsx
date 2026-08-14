77d30e90a180fc61fcc49942901a7ef85d9c0bbe
MarkBjj <markmullendev@gmail.com>
2026-08-14 10:34:53 -0300

    before button click listeners

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,15 @@ import ProjectsSidebar from "./components/ProjectsSideBar";
 //import BoxContent from "./components/BoxContent";
 import NewProject from "./components/NewProject";
 import NoProjectSelected from "./components/NoProjectSelected";
+import { useState } from "react";
 function App() {
+  //which component to show in the main content area
+  const [selectedProject, setSelectedProject] = useState({
+    selectedProject: undefined,
+  });
+
   return (
-    <main className="flex h-screen w-full my-8 gap-4 bg-gray-200">
+    <main className="flex h-screen w-full my-6 gap-4 bg-gray-200">
       <ProjectsSidebar />
       <NoProjectSelected />
     </main>
--- src/components/ProjectsSideBar.jsx
+++ src/components/ProjectsSideBar.jsx
@@ -2,7 +2,7 @@ import NewProject from "./NewProject";
 import Button from "./Buttons.jsx";
 export default function ProjectsSideBar() {
   return (
-    <aside className="w-1/4  border-x-8 px-8 py-16 bg-stone-700 md:w-72 rounded-r-xl">
+    <aside className="w-1/5  border-x-8 px-8 py-16 bg-stone-700 md:w-72 rounded-r-xl">
       <h3 className="mb-2 font-bold text-stone-300 uppercase md:text-xl">
         My Project List
       </h3>
